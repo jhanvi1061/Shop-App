18f47b06049c232a517b967b0c083f700d388110
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-05-13 19:00:52 +0530

    updated products

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -6,11 +6,11 @@ class ProductsProvider with ChangeNotifier {
   List<Product> _items = [
     Product(
       id: 'p1',
-      title: 'Blue T-Shirt',
-      description: 'A blue tshirt - it is pretty blue!',
+      title: 'Red T-Shirt',
+      description: 'A red tshirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2020/06/04/06/23/mockup-5257442__340.jpg',
+          'https://cdn.pixabay.com/photo/2020/06/04/06/23/mockup-5257444__340.jpg',
     ),
     Product(
       id: 'p2',
@@ -22,11 +22,11 @@ class ProductsProvider with ChangeNotifier {
     ),
     Product(
       id: 'p3',
-      title: 'Red Tshirt',
-      description: 'A red tshirt - it is pretty red!',
+      title: 'Shoes',
+      description: 'Comfy shoes.',
       price: 19.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2020/06/04/06/23/mockup-5257444__340.jpg',
+          'https://cdn.pixabay.com/photo/2013/07/12/18/20/shoes-153310_960_720.png',
     ),
     Product(
       id: 'p4',
@@ -37,36 +37,68 @@ class ProductsProvider with ChangeNotifier {
           'https://d3uvb2lhumlp.cloudfront.net/pub/media/catalog/product/cache/f751fae08115cd00f88855057a9f034e/t/h/th9190_fsl_24_1.jpg',
     ),
     Product(
-      id: 'p1',
-      title: 'Blue T-Shirt',
-      description: 'A blue tshirt - it is pretty blue!',
+      id: 'p5',
+      title: "Men's Watch",
+      description: 'Perfect watch to get a royal look in  hand.',
       price: 29.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2020/06/04/06/23/mockup-5257442__340.jpg',
+          'https://cdn.pixabay.com/photo/2014/07/31/23/00/wristwatch-407096_960_720.jpg',
     ),
     Product(
-      id: 'p2',
-      title: 'Baby Jacket',
-      description: 'Warm and cozy.',
+      id: 'p6',
+      title: 'Reading Glasses',
+      description: 'Get a funky look wearing this.',
       price: 59.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206354__340.jpg',
+          'https://cdn.pixabay.com/photo/2016/03/09/15/21/glasses-1246611_960_720.jpg',
     ),
     Product(
-      id: 'p3',
-      title: 'Red Tshirt',
-      description: 'A red tshirt - it is pretty red!',
+      id: 'p7',
+      title: 'Jeans',
+      description: 'Funky Jeans.',
       price: 19.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2020/06/04/06/23/mockup-5257444__340.jpg',
+          'https://cdn.pixabay.com/photo/2014/05/21/14/54/feet-349687_960_720.jpg',
     ),
     Product(
-      id: 'p4',
-      title: 'Tshirt',
-      description: 'Simple tshirt to keep you away from summer heat.',
+      id: 'p8',
+      title: 'Macbook Air',
+      description: 'Enhance your workplace with the new MacBook Air Model.',
       price: 49.99,
       imageUrl:
-          'https://d3uvb2lhumlp.cloudfront.net/pub/media/catalog/product/cache/f751fae08115cd00f88855057a9f034e/t/h/th9190_fsl_24_1.jpg',
+          'https://cdn.pixabay.com/photo/2014/05/02/21/50/laptop-336378_960_720.jpg',
+    ),
+    Product(
+      id: 'p9',
+      title: 'Winter Jacket',
+      description: 'Save yourself from winter cold.',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2015/03/26/09/54/person-690547_960_720.jpg',
+    ),
+    Product(
+      id: 'p10',
+      title: 'Makeup Accessories',
+      description: 'Perfect set of brushes to add to your makeup wardrobe.',
+      price: 59.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/02/19/11/35/makeup-1209798_960_720.jpg',
+    ),
+    Product(
+      id: 'p11',
+      title: 'Night Lamp',
+      description: 'Light your night.',
+      price: 19.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/08/18/20/05/light-bulbs-1603766_960_720.jpg',
+    ),
+    Product(
+      id: 'p12',
+      title: 'Bluetooth Speaker',
+      description: 'Perfect to listen music at high volume.',
+      price: 49.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2019/04/07/09/41/speakers-4109274_960_720.jpg',
     ),
   ];
 
