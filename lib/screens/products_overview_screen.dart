c33ccedfc56eb2bb3fdcf0201d55d83f01299b00
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-17 22:06:36 +0530

    fetched data n transformed it

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:provider/provider.dart';
 
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 import '../providers/cart.dart';
 import '../screens/cart_screen.dart';
@@ -17,6 +18,28 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    // Future.delayed(Duration.zero).then(
+    //   (_) => Provider.of<ProductsProvider>(context).fetchAndSetProducts(),
+    // );
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() => _isLoading = true);
+      Provider.of<ProductsProvider>(context).fetchAndSetProducts().then((_) {
+        setState(() => _isLoading = false);
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +103,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               ],
             ),
             drawer: screenWidth < 600 ? AppDrawer() : null,
-            body: ProductsGrid(_showOnlyFavorites),
+            body: _isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ProductsGrid(_showOnlyFavorites),
           ),
         ),
       ],
