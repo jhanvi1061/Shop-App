8c72abe844d5125132dff042add1d9f9705367b7
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-17 22:24:17 +0530

    updating products done using patch method

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -211,9 +211,20 @@ class ProductsProvider with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://by-jhanvi-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(
+        url,
+        body: json.encode({
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'imageUrl': newProduct.imageUrl,
+          'price': newProduct.price,
+        }),
+      );
       _items[prodIndex] = newProduct;
       notifyListeners();
     }
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -95,10 +95,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != null) {
-      Provider.of<ProductsProvider>(context, listen: false)
+      await Provider.of<ProductsProvider>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() => _isLoading = false);
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<ProductsProvider>(context, listen: false)
@@ -117,11 +115,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() => _isLoading = false);
-        Navigator.of(context).pop();
       }
+      // finally {
+      //   setState(() => _isLoading = false);
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() => _isLoading = false);
+    Navigator.of(context).pop();
   }
 
   @override
