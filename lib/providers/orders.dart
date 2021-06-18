e52a009059ad617f870ffe16f0672c9c82fea33d
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-18 18:50:26 +0530

    fetching orders

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -24,9 +24,41 @@ class Orders with ChangeNotifier {
 
   List<OrderItem> get orders => [..._orders];
 
+  Future<void> fetchAndSetOrders() async {
+    final url =
+        Uri.parse('https://by-jhanvi-default-rtdb.firebaseio.com/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          datetTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  id: item['id'],
+                  title: item['title'],
+                  quantity: item['quantity'],
+                  price: item['price'],
+                ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    final url = Uri.parse(
-        'https://by-jhanvi-default-rtdb.firebaseio.com/orders.json');
+    final url =
+        Uri.parse('https://by-jhanvi-default-rtdb.firebaseio.com/orders.json');
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,8 +6,25 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() => _isLoading = true);
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() => _isLoading = false);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +41,13 @@ class OrdersScreen extends StatelessWidget {
         ),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        itemCount: orderData.orders.length,
-        itemBuilder: (context, i) => OrderItem(orderData.orders[i]),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: orderData.orders.length,
+              itemBuilder: (context, i) => OrderItem(orderData.orders[i]),
+            ),
     );
   }
 }
