2d4f62301e96de9c887bae27a0ae874c5400e844
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-30 22:18:54 +0530

    animating containers

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -20,53 +20,64 @@ class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     print("Build() - OrderItem");
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            tileColor: const Color(0xffFFFAF4),
-            title: Text("\$${widget.order.amount}"),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.datetTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() => _expanded = !_expanded);
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              color: const Color(0xffFFFAF4),
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 20, 100),
-              child: ListView(
-                children: widget.order.products
-                    .map((prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              prod.title,
-                              style: const TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            Text(
-                              "${prod.quantity}x \$${prod.price}",
-                              style: const TextStyle(
-                                fontSize: 15,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ))
-                    .toList(),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              tileColor: const Color(0xffFFFAF4),
+              title: Text("\$${widget.order.amount}"),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.datetTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() => _expanded = !_expanded);
+                },
               ),
             ),
-        ],
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 110, 200)
+                  : 0,
+              child: Container(
+                color: const Color(0xffFFFAF4),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                // height: min(widget.order.products.length * 20.0 + 110, 200),
+                child: ListView(
+                  children: widget.order.products
+                      .map((prod) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                prod.title,
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                "${prod.quantity}x \$${prod.price}",
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
